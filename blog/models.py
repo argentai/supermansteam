909a8e727ec1f91d4da8c52a3579f32483232e50
argentai <argentai.kaly@gmail.com>
2022-02-17 14:57:39 +0600

    feature/task3/add_ckeditor/add_grappelli/

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+s = [
+    ('Private','Private'),
+    ('Public','Public')
+]
 
-# Create your models here.
+class dospital(models.Model):
+    region_kg = models.CharField(max_length=50)
+    person_quantity = models.DecimalField(max_digits=100, decimal_places=0) #нужно поменять
+    public_or_private = models.CharField(max_length=15, choices=s)
+    okpo = models.CharField(max_length=1000, unique=True)
+
+    # def __str__(self):
+    #     return self.okpo
+
+class medical_officer(models.Model):
+    full_name = models.CharField(max_length=90)
+    pincode_passport = models.IntegerField()
+    age = models.DateTimeField()
+    work_experience = models.TextField()
+    phone_number = models.IntegerField()
+
+
