931eed0f64a695660cc23f2e6f47062411df7695
argentai <argentai.kaly@gmail.com>
2022-02-18 14:23:37 +0600

    feature/task3/add_ckeditor/add_grappelli/

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(dospital)
-admin.site.register(medical_officer)
+admin.site.register(Hospital)
+admin.site.register(GlavVrach)
+admin.site.register(Lech_vrach)
+admin.site.register(Medsestra)
+admin.site.register(Pacient)
--- blog/models.py
+++ blog/models.py
@@ -1,24 +1,49 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-s = [
-    ('Private','Private'),
-    ('Public','Public')
+# from django.contrib.auth import get_user_model
+region = [
+    ('Bishkek','Bishkek'),
+    ('Osh','Osh'),
+    ('Naryn','Naryn')
 ]
 
-class dospital(models.Model):
-    region_kg = models.CharField(max_length=50)
+class Hospital(models.Model):
+    region_kg = models.CharField(max_length=50, choices=region)
     person_quantity = models.DecimalField(max_digits=100, decimal_places=0) #нужно поменять
-    public_or_private = models.CharField(max_length=15, choices=s)
+    public_or_private = models.BooleanField(default=False)
     okpo = models.CharField(max_length=1000, unique=True)
 
-    # def __str__(self):
-    #     return self.okpo
+class GlavVrach(models.Model):
+    full_name = models.CharField(max_length=90, blank=True, null=True)
+    pincode_passport = models.DecimalField(max_digits=4, decimal_places=0, default="0000")
+    age = models.IntegerField("Возраст", default=0)
+    work_experience = models.TextField(blank=False)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
 
-class medical_officer(models.Model):
-    full_name = models.CharField(max_length=90)
-    pincode_passport = models.IntegerField()
-    age = models.DateTimeField()
-    work_experience = models.TextField()
-    phone_number = models.IntegerField()
+p = [
+    ('Terapevt','Terapevt'),
+    ('Hirurg','Hirurg')
+]
+
+class Lech_vrach(models.Model):
+    napravlenie = models.CharField(max_length=200, blank=True, null=True)
+    vybor = models.CharField(max_length=100, choices=p, default='none')
+    full_name = models.CharField(max_length=90, blank=True, null=True)
+    pincode_passport = models.DecimalField(max_digits=4, decimal_places=0,default="0000")
+    age = models.IntegerField("Возраст", default=0)
+    work_experience = models.TextField(blank=False)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+
+class Medsestra(models.Model):
+    full_name = models.CharField(max_length=90, blank=True, null=True)
+    pincode_passport = models.DecimalField(max_digits=4, decimal_places=0, default="0000")
+    age = models.IntegerField("Возраст", default=0)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+
+class Pacient(models.Model):
+    full_name = models.CharField(max_length=90, blank=True, null=True)
+    pincode_passport = models.DecimalField(max_digits=4, decimal_places=0, default="0000")
+    age = models.IntegerField("Возраст", default=0)
+    phone_number = models.CharField(max_length=10, blank=True, null=True)
+    reason = models.TextField(blank=True)
 
 
